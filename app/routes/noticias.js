6a6acb50d3b10bdd78cfebf619bfc93d986e9407
Cleyton Souza <souza.cleyton@gmailcom>
2018-07-30 22:49:24 -0300

    Parcial 02

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,19 @@
 
 module.exports = function(app) {
+  
     app.get('/noticias', function(req,res){
-        res.render("noticias/noticia");
+        let mysql = require('mysql');
+
+        let connection = mysql.createConnection({
+            host: '52.71.244.54',
+            user: 'mondamon',
+            password:'123456',
+            database: 'portal_noticias',
+        });
+    
+        connection.query("select * from noticias",function(error,result){
+                res.send(result);
+        });
+       // res.render("noticias/noticia");
 });
 };
--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let app = express();
+let consign = require('consign');
+
 app.set('view engine', 'ejs')
 
 app.set('views','./app/views');
+
+consign().include('app/routes').into(app);
 
 module.exports = app;
